0d727b4b27c48c131009f42b2e2bb573188b29bb
SnehilSundriyal <snehilsundriyal@gmail.com>
2025-06-26 03:29:58 +0530

    refresh tokens done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sf: ['"SF Pro Semibold"', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
